9be6d18bc2748e929c42f6b5a927410c3c91e063
mozaru <mozar.silva@gmail.com>
2022-05-21 10:53:00 -0300

    somente minusculas para interface

---
+++ utils/LowerCaseNamingPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Prisma
+{
+    public class LowerCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            return name.ToLowerInvariant();
+        }
+    }
+}
